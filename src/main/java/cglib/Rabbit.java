b179a1734df555b7ff363bfc5a962a24d142b72d
suhongkim98 <kshz9815@naver.com>
2022-01-26 14:52:59 +0900

    동물 클래스 작성

---
+++ src/main/java/cglib/Rabbit.java
@@ -0,0 +1,11 @@
+package cglib;
+
+public class Rabbit {
+    public void eat() {
+        System.out.println("토끼가 음식을 먹습니다.");
+    }
+
+    public void drink() {
+        System.out.println("토끼가 물을 마십니다.");
+    }
+}
