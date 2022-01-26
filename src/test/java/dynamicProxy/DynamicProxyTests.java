7805e1f01a270ee4ad855eb69492410d7520329c
suhongkim98 <kshz9815@naver.com>
2022-01-26 11:17:27 +0900

    dynamic proxy example 테스트코드 작성

--- src/test/java/dynamicProxy/DynamicProxyTests.java
+++ src/test/java/dynamicProxy/DynamicProxyTests.java
@@ -7,20 +7,16 @@ import java.lang.reflect.Proxy;
 public class DynamicProxyTests {
     @Test
     public void dynamicProxyExample() {
-        // Create the target instance
-        Animal rabbitInstance = new Rabbit();
-        Animal tigerInstance = new Tiger();
-
         // Create the proxy
         //동적으로 프록시 생성
         // rabbit
         Animal rabbit = (Animal) Proxy.newProxyInstance(Animal.class.getClassLoader(),
-                rabbitInstance.getClass().getInterfaces(),
-                new AnimalProxyHandler(rabbitInstance));
+                new Class[]{Animal.class},
+                new AnimalProxyHandler(new Rabbit()));
         // tiger
         Animal tiger = (Animal) Proxy.newProxyInstance(Animal.class.getClassLoader(),
-                tigerInstance.getClass().getInterfaces(),
-                new AnimalProxyHandler(tigerInstance));
+                new Class[]{Animal.class},
+                new AnimalProxyHandler(new Tiger()));
 
         // Invoke the target instance method through the proxy
         rabbit.eat();
@@ -43,8 +39,45 @@ public class DynamicProxyTests {
         // Create the proxy
         //동적으로 프록시 생성하는데 핸들러를 람다로 구현
         Animal rabbit = (Animal) Proxy.newProxyInstance(Animal.class.getClassLoader(),
-                rabbitInstance.getClass().getInterfaces(),
+                new Class[]{Animal.class},
                 (proxy, method, args) -> {
+                    Object result = null;
+                    System.out.println("****before****");
+
+                    //메서드가 eat 이라면
+                    if(method.getName().equals("eat")) {
+                        System.out.println("----eat 메서드 호출 전----");
+
+                        result = method.invoke(rabbitInstance, args); // 메서드 호출
+
+                        System.out.println("----eat 메서드 호출 후----");
+                    } else if(method.getName().equals("drink")) { // 메서드가 drink 라면
+                        System.out.println("----drink 메서드 호출 전----");
+
+                        result = method.invoke(rabbitInstance, args); // 메서드 호출
+
+                        System.out.println("----drink 메서드 호출 후----");
+                    }
+
+                    System.out.println("****after****");
+                    return result; // 호출결과 반환
+                });
+
+        // Invoke the target instance method through the proxy
+        rabbit.eat();
+        System.out.println();
+        rabbit.drink();
+        System.out.println();
+    }
+    @Test
+    public void dynamicProxyExampleInJava8Lambda_InstanceInHandler() {
+        //핸들러를 람다식으로 표현할 수 있다.
+        // Create the proxy
+        //동적으로 프록시 생성하는데 핸들러를 람다로 구현
+        Animal rabbit = (Animal) Proxy.newProxyInstance(Animal.class.getClassLoader(),
+                new Class[]{Animal.class},
+                (proxy, method, args) -> {
+                    Animal rabbitInstance = new Rabbit();
                     Object result = null;
                     System.out.println("****before****");
 
